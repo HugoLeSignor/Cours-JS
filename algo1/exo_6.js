e7d4cd28d070f56592b16aa4c2281682e4b75213
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-12 14:39:02 +0100

    [ADD] exo_6

---
+++ algo1/exo_6.js
@@ -0,0 +1,11 @@
+// 6.Fontion pair
+// Faire une programme qui vérifie si le nombre d'un utilisateur est pair
+
+let nombre = 10; // L'utilisateur peut changer ce nombre
+
+if (nombre % 2 === 0) {
+    console.log(nombre + " est pair");
+} else {
+    console.log(nombre + " est impair");
+}
+
