c4462e5ed2463c81d8cebd13fbcbe1b35b9f6184
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-12 13:54:10 +0100

    [ADD] Implemented ascending and descending loops

---
+++ algo1/boucleCroissanteBIS.js
@@ -0,0 +1,8 @@
+// 1. Boucle croissante bis
+// Afficher les 25 premiers entiers (de 0 à 24) avec une boucle TANT_QUE (while)
+
+let i = 0;
+while (i < 25) {
+    console.log(i);
+    i++;
+}
---
+++ algo1/boucleDecroissante.js
@@ -0,0 +1,6 @@
+// 2.Boucle décroissante
+// Affichez dans l'ordre décroissant les 25 premiers entiers (de 25 à 1, sans 0).
+
+for (let i = 25; i >= 1; i--) {
+    console.log(i);
+}
