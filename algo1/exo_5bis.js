d340c2dd9430f6ebabce5d10099855fefeb9c804
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-12 14:35:33 +0100

    [ADD] exo_5bis.js

--- algo1/exo_5bis.js
+++ algo1/exo_5bis.js
@@ -1,3 +1,12 @@
 // 5-bis.Somme multiple personnalisé
 // Utilisez l'exercice précédent afin de laisser l'utilisateur choisir le nombre à calculer
 
+let n = 30; // L'utilisateur peut changer ce nombre
+
+let somme = 0;
+
+for (let i = 1; i <= n; i++) {
+    somme += i;
+}
+
+console.log(somme);
