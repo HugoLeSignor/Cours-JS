61df9ba2717484bae7501a53005d9838c7244aba
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-12 14:58:42 +0100

    [ADD] exo_8

---
+++ algo1/exo_8.js
@@ -0,0 +1,13 @@
+// 8.Fonction pythagore
+// Faire un programme qui permet de trouver l'hypothénuse d'un triangle rectange.
+// L'utilisateur devra rentrer les longueurs b et c
+// Pour rappel le théorème de Pythagore qui permet de calculer l'hypothénuse est :
+// a²=b²+c²
+
+let b = 3; // L'utilisateur peut changer cette valeur
+let c = 4; // L'utilisateur peut changer cette valeur
+
+let a = Math.sqrt(b * b + c * c);
+
+console.log(a);
+
