9c77848ad0c70bea7fcec94214274cf23f14f5f0
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-12 14:50:31 +0100

    [ADD] exo_7

---
+++ algo1/exo_7.js
@@ -0,0 +1,8 @@
+// 7.Boucle pair
+// Utiliser le programme précédent pour afficher uniquement les nombres pair de 1 a 20
+
+for (let i = 1; i <= 25; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
