d02a365fcf9abb672e5aedcf34d8e50cbe353d45
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-13 09:15:59 +0100

    [ADD] exo_11 had to look on internet to make the table

--- algo1/exo_11.js
+++ algo1/exo_11.js
@@ -1,9 +1,10 @@
 // 11.Table des multiplications
 // Faire le tableau des multiplications de 0 à 9
-
+// Tables de multiplication
 for (let i = 0; i <= 9; i++) {
-    console.log(`\nTable de ${i} :`);
-    for (let j = 0; j <= 9; j++) {
-        console.log(`${i} x ${j} = ${i * j}`);
-    }
+  let ligne = `${i}`
+  for (let j = 0; j <= 9; j++) {
+    ligne += `${(i * j).toString().padStart(2)}`
+  }
+  console.log(ligne)
 }
