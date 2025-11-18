80ed4396365a8d4bf2a37635b66d606ed7001649
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-18 14:06:42 +0100

    [ADD] Filter exercice for DOM manipulation

---
+++ dom/dom_4/script.js
@@ -0,0 +1,17 @@
+const filterInput = document.getElementById('filterInput')
+const countryList = document.getElementById('countryList')
+const countries = countryList.querySelectorAll('li')
+
+filterInput.addEventListener('input', function() {
+    const filterText = filterInput.value.toLowerCase()
+    
+    countries.forEach(function(country) {
+        const countryName = country.textContent.toLowerCase()
+        
+        if(countryName.includes(filterText)) {
+            country.style.display = ''
+        } else {
+            country.style.display = 'none'
+        }
+    })
+})
