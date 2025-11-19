2c30489fd29bfca8c8f7e3d8e704c27dc9ed41cd
Hugo Le Signor <hugo.lesignor@gmail.com>
2025-11-19 11:22:21 +0100

    [FIX] changed links and removed a useless line in js for dom_3

--- dom/dom_3/script.js
+++ dom/dom_3/script.js
@@ -3,7 +3,6 @@ const thumbnails = document.querySelectorAll('.thumbnail')
 
 thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener('click', function() {
-        mainImage.src = thumbnail.src.replace('200/150', '600/400')
-        mainImage.alt = thumbnail.alt
+        mainImage.src = thumbnail.src
     })
 })
